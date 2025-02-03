6cb0fc4a46e58cd7c6ad2ef7c003eaaed388b14d
MatthieuMordrel <matthieumordrel@gmail.com>
2025-02-03 17:31:07 +0100

    processed local prod + elements

--- main.py
+++ main.py
@@ -17,18 +17,20 @@ PRODUCT_MAPPING_PATH = Path("Mappings/product_mapping.json")
 def get_test_batch() -> List[BatchRequest]:
     """Returns a predefined test batch of requests."""
     return [
-        ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D19"), #Simple Multiplication =+C19*D17
-        ("2022 - P1 Berekening opzetkast 1323-KLEUR.xlsx", "OVERZICHT COZ1323", "G33"), #Multiplication & external reference to base material
-        ("2022 -P2 Berekening 2b Ladenkasten 794-KLEUR.xlsx", "OVERZICHT COP", "F65"), #3 external references
-        ("2022 - P6 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "G17"), #SUM + 2 internal references
-        ("2022 - P6 berekening kolomkast 2137.xlsx", "DE446x2137", "H39"), #Element
-        ("2022 - P6 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "E17"), #Sum of 2 elements
-        ("2022 - P1 Berekening  Ladenkasten 794-KLEUR.xlsx", "LADE 30", "R36"), #Division
-        ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D35"), #Element + External reference
-        ("2022 - P1 Berekening  Ladenkasten 794-KLEUR.xlsx", "PLADE 30", "W18"), #Element + Base material reference
-        ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "B28"), #Element + Division weird stuff
-        ("2022 - P5 berekening kolomkast 2317.xlsx", "OVERZICHT CK231", "D17"), #Product Kost
+        # ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D19"), #Simple Multiplication =+C19*D17
+        # ("2022 - P1 Berekening opzetkast 1323-KLEUR.xlsx", "OVERZICHT COZ1323", "G33"), #Multiplication & external reference to base material
+        # ("2022 -P2 Berekening 2b Ladenkasten 794-KLEUR.xlsx", "OVERZICHT COP", "F65"), #3 external references
+        # ("2022 - P6 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "G17"), #SUM + 2 internal references
+        # ("2022 - P6 berekening kolomkast 2137.xlsx", "DE446x2137", "H39"), #Element
+        # ("2022 - P6 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "E17"), #Sum of 2 elements
+        # ("2022 - P1 Berekening  Ladenkasten 794-KLEUR.xlsx", "LADE 30", "R36"), #Division
+        # ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D35"), #Element + External reference
+        # ("2022 - P1 Berekening  Ladenkasten 794-KLEUR.xlsx", "PLADE 30", "W18"), #Element + Base material reference
+        # ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "B28"), #Element + Division weird stuff
+        # ("2022 - P5 berekening kolomkast 2317.xlsx", "OVERZICHT CK231", "D17"), #Product Kost
+        ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D35"), #Error
     ]
+
 
 def main():
     # Initialize components
