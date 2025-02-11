d72b913df4610dc523075fb1abf8e15138897acd
MatthieuMordrel <matthieumordrel@gmail.com>
2025-02-11 12:53:47 +0100

    added all kost

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from cell_info_extractor import CellInfoExtractor
 from typing import List
 
 # Configuration
-USE_BATCH_FILE = False
+USE_BATCH_FILE = True
 BATCH_FILE_PATH = Path(__file__).parent / "Batch File" / "File - Tab - Cell - (start of recursive resolver) - New.xlsx"
 BASE_PATH = Path(r"C:\Users\matth\OneDrive - Matthieu Mordrel\Work\Projects\Kovera\Project 2\BASISMATERIALEN")
 LOG_PATH = Path("Logs/log.json")
@@ -17,37 +17,37 @@ PRODUCT_MAPPING_PATH = Path("Mappings/product_mapping.json")
 def get_test_batch() -> List[BatchRequest]:
     """Returns a predefined test batch of requests."""
     return [
-        ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D19"), #Simple Multiplication =+C19*D17
-        ("2022 - P1 Berekening opzetkast 1323-KLEUR.xlsx", "OVERZICHT COZ1323", "G33"), #Multiplication & external reference to base material
-        ("2022 -P2 Berekening 2b Ladenkasten 794-KLEUR.xlsx", "OVERZICHT COP", "F65"), #3 external references
-        ("2022 - P6 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "G17"), #SUM + 2 internal references
-        ("2022 - P6 berekening kolomkast 2137.xlsx", "DE446x2137", "H39"), #Element
-        ("2022 - P6 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "E17"), #Sum of 2 elements
-        ("2022 - P1 Berekening  Ladenkasten 794-KLEUR.xlsx", "LADE 30", "R36"), #Division
-        ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D35"), #Element + External reference
-        ("2022 - P1 Berekening  Ladenkasten 794-KLEUR.xlsx", "PLADE 30", "W18"), #Element + Base material reference
-        ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "B28"), #Element + Division weird stuff
-        ("2022 - P5 berekening kolomkast 2317.xlsx", "OVERZICHT CK231", "D17"), #Product Kost
-        ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D35"), #Error
+        # ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D19"), #Simple Multiplication =+C19*D17
+        # ("2022 - P1 Berekening opzetkast 1323-KLEUR.xlsx", "OVERZICHT COZ1323", "G33"), #Multiplication & external reference to base material
+        # ("2022 -P2 Berekening 2b Ladenkasten 794-KLEUR.xlsx", "OVERZICHT COP", "F65"), #3 external references
+        # ("2022 - P6 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "G17"), #SUM + 2 internal references
+        # ("2022 - P6 berekening kolomkast 2137.xlsx", "DE446x2137", "H39"), #Element
+        # ("2022 - P6 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "E17"), #Sum of 2 elements
+        # ("2022 - P1 Berekening  Ladenkasten 794-KLEUR.xlsx", "LADE 30", "R36"), #Division
+        # ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D35"), #Element + External reference
+        # ("2022 - P1 Berekening  Ladenkasten 794-KLEUR.xlsx", "PLADE 30", "W18"), #Element + Base material reference
+        # ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "B28"), #Element + Division weird stuff
+        # ("2022 - P5 berekening kolomkast 2317.xlsx", "OVERZICHT CK231", "D17"), #Product Kost
+        # ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D35"), #Error
 
-        #  Products Cost
-        ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D17"), #Elements
-        ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D35"), #Elements + Local Products
-        ("2022 -P1 Berekening Kolom P Lade B Lade(16-10) 2137-KLEUR.xlsx", "OVERZICHT CKP222", "D22"), #Local Products + External products
-        ("2022 -P1 Berekening Kolom P Lade B Lade(16-10) 2137-KLEUR.xlsx", "OVERZICHT CKP222", "D3"), #Elements + External Products
-        ("2022 -P1 Berekening Kolom P Lade F+O (11-12-14-17-18-19-20-21-23) 2137-KLEUR.xlsx", "OVERZICHT CKP222", "G3"), #Local products
-        # ("2022 -P4 Berekening  Ladenkasten 794-KLEUR.xlsx", "OVERZICHT COP", "D11"), #Elements + Hardcoded
-        # ("2022 - P1 Berekening  Ladenkasten 794-KLEUR.xlsx", "OVERZICHT COP", "M35"), #Local Products + ALU BODEM - Hardcoded value
-        ("2022 - P1 Berekening  Ladenkasten 794-KLEUR.xlsx", "OVERZICHT COP", "D65"), #Elements + Multiplied Elements
-        ("2022 -P2 Berekening 2b Ladenkasten 794-KLEUR.xlsx", "OVERZICHT COP", "F65"), #External Products
-        # ("2022 -P1 Berekening FLES-LFR - PL 794-KLEUR.xlsx", "OVERZICHT PO", "G3"), #Mistake
+        # #  Products Cost
+        # ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D17"), #Elements
+        # ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D35"), #Elements + Local Products
+        # ("2022 -P1 Berekening Kolom P Lade B Lade(16-10) 2137-KLEUR.xlsx", "OVERZICHT CKP222", "D22"), #Local Products + External products
+        # ("2022 -P1 Berekening Kolom P Lade B Lade(16-10) 2137-KLEUR.xlsx", "OVERZICHT CKP222", "D3"), #Elements + External Products
+        # ("2022 -P1 Berekening Kolom P Lade F+O (11-12-14-17-18-19-20-21-23) 2137-KLEUR.xlsx", "OVERZICHT CKP222", "G3"), #Local products
+        # # ("2022 -P4 Berekening  Ladenkasten 794-KLEUR.xlsx", "OVERZICHT COP", "D11"), #Elements + Hardcoded
+        # # ("2022 - P1 Berekening  Ladenkasten 794-KLEUR.xlsx", "OVERZICHT COP", "M35"), #Local Products + ALU BODEM - Hardcoded value
+        # ("2022 - P1 Berekening  Ladenkasten 794-KLEUR.xlsx", "OVERZICHT COP", "D65"), #Elements + Multiplied Elements
+        # ("2022 -P2 Berekening 2b Ladenkasten 794-KLEUR.xlsx", "OVERZICHT COP", "F65"), #External Products
+        # # ("2022 -P1 Berekening FLES-LFR - PL 794-KLEUR.xlsx", "OVERZICHT PO", "G3"), #Mistake
 
-        # product Prices
-        ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D19"),
-        ("2022 - P2 Berekening Open Kolomkast 2227-KLEUR.xlsx", "OVERZICHT COK222", "D5"),
-        ("2022 -P2 Berekening Kolom F+O (13-22-24-31-32-33-35-37+38+39) 2227-KLEUR.xlsx", "OVERZICHT CK222", "H3"),
-        ("2022 - P1 Berekening klapdeurHangkast 494-565 KLEUR.xlsx", "OVERZICHT C494", "D5"), #Standard + Material
-        ("2022 - P1 Berekening GLAS DEUR Hangkast 970-KLEUR.xlsx", "OVERZICHT C79B", "D41"), #Standard + Hardocded
+        # # product Prices
+        # ("2022 - P1 berekening kolomkast 2137.xlsx", "OVERZICHT CK213", "D19"),
+        # ("2022 - P2 Berekening Open Kolomkast 2227-KLEUR.xlsx", "OVERZICHT COK222", "D5"),
+        # ("2022 -P2 Berekening Kolom F+O (13-22-24-31-32-33-35-37+38+39) 2227-KLEUR.xlsx", "OVERZICHT CK222", "H3"),
+        # ("2022 - P1 Berekening klapdeurHangkast 494-565 KLEUR.xlsx", "OVERZICHT C494", "D5"), #Standard + Material
+        # ("2022 - P1 Berekening GLAS DEUR Hangkast 970-KLEUR.xlsx", "OVERZICHT C79B", "D41"), #Standard + Hardocded
         ("2022 - P1 Berekening GLAS DEUR Hangkast 970-KLEUR.xlsx", "OVERZICHT C79B", "D33"),
         ("2022 - P1 Berekening Hoekonderkasten 794-KLEUR.xlsx", "OVERZICHT CO", "F5"),
 
