03ab14afe9dee348827d9bd0d034af8487876f7c
MatthieuMordrel <matthieumordrel@gmail.com>
2025-04-23 11:38:39 +0200

    added pieter products

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from typing import List
 # Configuration
 USE_BATCH_FILE = True
 BATCH_FILE_PATH = Path(__file__).parent / "Batch File" / "File - Tab - Cell - (start of recursive resolver) - New.xlsx"
-BASE_PATH = Path(r"C:\Users\matth\OneDrive - Matthieu Mordrel\Work\Projects\Kovera\Project 2\BASISMATERIALEN")
+BASE_PATH = Path(r"C:\Users\matth\OneDrive - Matthieu Mordrel\Work\Projects\Kovera\Project 2\Analysis of Files\New Product Files")
 LOG_PATH = Path("Logs/Current Logs/log.json")
 PRODUCT_MAPPING_PATH = Path("Mappings/product_mapping.json")
 
